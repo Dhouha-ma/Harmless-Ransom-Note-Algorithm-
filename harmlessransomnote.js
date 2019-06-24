c08e5377ed8bf50d138da654da2433b0a670c83c
Dhouha Mansour <dhouha.mansour11@gmail.com>
2019-06-24 22:18:52 +0200

    Update harmlessransomnote.js

--- harmlessransomnote.js
+++ harmlessransomnote.js
@@ -1,14 +1,3 @@
-/* 
-  
-Harmless Ransom Note Algorithm 
-  
-It's a note made from words cut out of a magazine. 
-The function will take two parameters ' noteText' and 'magazineText' 
-The purpose is to check if we have enough words in our magazine to create the note. 
-if we do, the algorithm should return true, if not it will return false. 
-  
-*/ 
-  
 function harmlessRansomNote (noteText, magazineText) { 
         var noteArray = noteText.split(' '); 
         var magazineArray = magazineText.split(' '); 
